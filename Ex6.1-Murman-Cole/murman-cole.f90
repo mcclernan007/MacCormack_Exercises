8f0403b3444be20b5a7faf5ee21a181cf019cc64
McClernan <mccle274@enet-km-ws33.nfs.aem.umn.edu>
2023-09-07 11:20:50 -0500

    still learning f90. Need plotting. Py?

--- Ex6.1-Murman-Cole/murman-cole.f90
+++ Ex6.1-Murman-Cole/murman-cole.f90
@@ -1,39 +1,60 @@
 program murman_cole
+      implicit none 
       
-      integer, parameter :: k_int = SELECTED_INT_KIND(16) !general int, 16 digits
-      integer, parameter :: k_flt = SELECTED_REAL_KIND(9,10) !general float, 9 digits, max val 1e10
-      integer, parameter :: k_dub = SELECTED_REAL_KIND(20,200) !general "double", 20 digits, max val 1e200
+      integer, parameter :: kint = SELECTED_INT_KIND(16) !general int, 16 digits
+      integer, parameter :: kflt = SELECTED_REAL_KIND(9,10) !general float, 9 digits, max val 1e10
+      integer, parameter :: kdub = SELECTED_REAL_KIND(20,200) !general "double", 20 digits, max val 1e200
 
-      real(k_flt) :: pi
+      real(kflt) :: pi
       
-      real(k_flt) :: airfoil_x
-      real(k_flt) :: airfoil_y
-      real(k_flt) :: airfoil_goalTh
-      real(k_flt) :: airfoil_th
-      real(k_flt) :: airfoil_chord
+      real(kflt) :: airfoil_x
+      real(kflt) :: airfoil_y
+      real(kflt) :: airfoil_goalTh
+      real(kflt) :: airfoil_th
+      real(kflt) :: airfoil_chord
 
-      real(k_flt) :: circ_x
-      real(k_flt) :: circ_y
-      integer(k_int) :: circ_Npts
+      integer(kint), parameter :: circ_Npts = 200_kint
 
-      integer(k_int) :: idx !general indexes. Should only use in loops
-      integer(k_int) :: jdx
-      integer(k_int) :: kdx
+      real(kflt), dimension(circ_Npts) :: circ_tht
+      real(kflt) :: circ_dtht
+      real(kflt), dimension(circ_Npts) :: circ_x
+      real(kflt), dimension(circ_Npts) :: circ_y
+      real(kflt), dimension(circ_Npts,2) :: circ_xy
 
+      integer :: io
 
-      
-      pi = 3.141592653589793238462643383279502_k_flt
+      integer(kint) :: idx !general indexes. Should only use in loops
+      integer(kint) :: jdx
+      integer(kint) :: kdx
+    
+      pi = 3.141592653589793238462643383279502_kflt
+
 
       airfoil_goalTh = 0.06
       airfoil_chord = 1.0
 
-      !generate unit circle, shift by varied dy, scale st c=1, measure thickness. get close. Need array knowlege
-      circ_Npts = 200 
-     
-      circ_x = 
-      do idx=1, circ_Npts
-          
+  
+!generate unit quarter circle, shift by varied dy, scale st c=1, measure thickness. get close. Need array knowlege
+      circ_dtht = (pi/2_kflt)/(real(circ_Npts)-1_kflt)
+      circ_tht(1) = 0_kflt
+      do idx=2_kint, circ_Npts
+        circ_tht(idx) = circ_tht(idx-1)+circ_dtht
+      end do 
+
+      circ_x = 1*cos(circ_tht) 
+      circ_y = 1*sin(circ_tht)
+      circ_xy(:,1) = circ_x
+      circ_xy(:,2) = circ_y
+      
+      open (newunit=io, file="out.txt", status="replace", action="write")
+      do idx=1, circ_Npts  
+          write(io, *) circ_xy(idx,1),  circ_xy(idx,2)
       end do
+      
+      close(io)
+!      do idx=1, circ_Npts
+          
+!      end do
       
 
 

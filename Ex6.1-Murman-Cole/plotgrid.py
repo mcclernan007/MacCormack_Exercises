887815586ce1c1be3d9571c99887e41f977dcbb3
Paul_personal_laptop <mccle274@umn.edu>
2023-11-02 14:57:07 -0500

    added grid plot

--- Ex6.1-Murman-Cole/plotgrid.py
+++ Ex6.1-Murman-Cole/plotgrid.py
@@ -8,6 +8,31 @@ def plotNodes(xdata,ydata):
     plt.grid()
     plt.scatter(xdata,ydata)
     plt.show()
+def plotGrid(xNodes,yNodes):
+    #pretty limited to this type of grid, but good enough for now
+    #Very lazy implementation
+    myColor = "#0072BD"
+    myWidth = 0.5
+    xRaw = np.sort(np.unique(xNodes));
+    yRaw = np.sort(np.unique(yNodes));
+    
+    plt.figure()
+    #interior
+    for idx in list(range(1,len(xRaw)-1)):
+        for jdx in list(range(1,len(yRaw)-1)):
+            plt.plot(xRaw[idx-1:idx+2],yRaw[jdx]*np.array([1,1,1]),color=myColor,linewidth=myWidth)
+            plt.plot(xRaw[idx]*np.array([1,1,1]),yRaw[jdx-1:jdx+2],color=myColor,linewidth=myWidth)
+    #edges
+    for idx in list(range(1,len(xRaw))):
+        plt.plot(xRaw[idx-1:idx+1],yRaw[0]*np.array([1,1]),color=myColor,linewidth=myWidth)
+        plt.plot(xRaw[idx-1:idx+1],yRaw[-1]*np.array([1,1]),color=myColor,linewidth=myWidth)
+    for jdx in list(range(1,len(xRaw))):
+        plt.plot(xRaw[0]*np.array([1,1]),yRaw[jdx-1:jdx+1],color=myColor,linewidth=myWidth)
+        plt.plot(xRaw[-1]*np.array([1,1]),yRaw[jdx-1:jdx+1],color=myColor,linewidth=myWidth)
+                
+    #plt.scatter(xNodes,yNodes)
+    
+    plt.show()        
 #    x = 
     
 def readXYDataFile(filename):
@@ -22,5 +47,5 @@ def readXYDataFile(filename):
     return x,y
     
 xydatafile = sys.argv[1];
-xdata,ydata = readXYDataFile(xydatafile)
-plotNodes(xdata,ydata);
+xNodes,yNodes = readXYDataFile(xydatafile)
+plotGrid(xNodes,yNodes);

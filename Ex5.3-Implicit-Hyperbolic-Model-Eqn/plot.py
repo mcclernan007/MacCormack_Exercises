969ff45cba86f051ffc213cd962753f382e7cfd5
Paul_personal_laptop <mccle274@umn.edu>
2024-01-03 13:46:40 -0600

    finished ex5.3

---
+++ Ex5.3-Implicit-Hyperbolic-Model-Eqn/plot.py
@@ -0,0 +1,60 @@
+import sys
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+ 
+def readDataFile(filename):
+    with open(filename) as f:
+        lines = f.readlines()
+    
+    lines = lines[1:-1] #rm number points (first entry)
+    x = np.empty(len(lines));
+    u_IC = np.empty(len(lines));
+    u = np.empty(len(lines));
+    for line, idx in zip(lines,list(range(0,len(lines)))) :
+        curLine = line.split()
+        x[idx] = float(curLine[0])
+        u_IC[idx] = float(curLine[1])
+        u[idx] = float(curLine[2])
+    return x,u_IC,u
+def getExactSolution(iwhich):
+    if iwhich == 1:
+        x,u_IC,u = readDataFile("exact_soln1.dat")
+    elif iwhich == 2: 
+        x,u_IC,u = readDataFile("exact_soln2.dat")
+    else:
+        x,u_IC,u = readDataFile("exact_soln1.dat")
+    return u
+def plotResult(x,u_IC,u,u_exact,outFilePath):
+    plt.figure()
+    plt.plot(x,u_IC,color='#999999',linestyle='dashed')
+    plt.plot(x,u_exact,color='#999999',linestyle='dashdot')
+    plt.plot(x,u,marker='.')
+    plt.title(outFilePath)
+    plt.show()
+    plt.savefig(outFilePath)
+
+if len(sys.argv)>1: #specified individual
+    datafile = sys.argv[1];
+    
+    x,u_IC,u = readDataFile(datafile)
+    datafile.replace(".\\","")
+    fname = datafile.split(".")
+    plotResult(x,u_IC,u,u_IC, fname[0]+".png")
+    #u_exact = getExactSolution(1)
+    
+#print([x,u_IC,u])
+    
+else: #plot everything and save
+    files = os.listdir();
+    for file in files:
+        if ".dat" in file: 
+            x,u_IC,u = readDataFile(file)
+            if "CFL_2" in file:
+                u_exact = getExactSolution(2)
+            else:
+                u_exact = getExactSolution(1)
+            fnames = file.split(".");
+            plotResult(x,u_IC,u,u_exact,fnames[0]+".png")
+            
+#plotGrid(xNodes,yNodes);
--- Ex5.3-Implicit-Hyperbolic-Model-Eqn/wave_eqn_imp.f90
+++ Ex5.3-Implicit-Hyperbolic-Model-Eqn/wave_eqn_imp.f90
@@ -1,19 +1,108 @@
 !Method 4,5: Implicit alpha method. 
 !  alpha=1: Implicit Central
 !  alpha=1/2: Crank-Nicholson
-function implicit_alph (x, dt, N, u_IC, u_BC, c, alph) result(u_np1) 
+function implicit_alph (x, dt, N, u_IC, u_BC, c, alph) result(u_out) 
 	implicit none
-	
+
 	real, dimension(1:), intent(in) :: x, u_IC
 	real, intent(in) 				:: dt, u_BC, c, alph
 	integer, intent(in) 			:: N
-	real, dimension(size(x))		:: u_n,u_np1
+	real, dimension(size(x))		:: u_out,v!,f
+	real, dimension(size(x)+1)		:: gam,u_np1,u_n
+	!real, dimension(size(x),size(x)):: A
 	
-	integer :: idx, ndx !spacial, time indicies
+	real 							:: ai, bi, ci, ali, fi
+	
+	integer :: idx, jdx, ndx !spacial, time indicies
+	integer :: I
 	real :: dx !assumed uniform
+	
+	I = size(x)
 	
 	u_n = u_IC
 	dx = (x(2)-x(1))
+	
+	do ndx = 1,N
+		!step 1 (not stated, but ghost nodes?)
+		u_np1(I+1) = 0d0
+		v(I+1) = 0d0
+		gam(I+1) = 0d0
+		
+		!step 2. tridiagonal inv
+		idx = I
+		ai = 1d0 + c*dt/dx
+		bi = 0d0
+		ci = -c*dt/dx
+		ali = ai
+		!calc, store
+		
+		gam(idx) = ci/ali
+		v(idx) = ((u_n(idx))-bi*u_np1(idx+1))/ali
+		do idx = I-1,2,-1
+			!calc, dont store
+			ai = 1d0
+			bi = alph*(c*dt/(2d0*dx))
+			ci = -bi
+			ali = ai-bi*gam(idx+1)
+			
+			!calc, store
+			gam(idx) = ci/ali
+			fi = u_n(idx)-(1-alph)*c*dt/(2*dx)*(u_n(idx+1)-u_n(idx-1))
+			v(idx) = (fi-bi*v(idx+1))/ali
+		end do
+		
+		!calc, dont store
+		idx = 1
+		ai = 1
+		bi = 0
+		ci = 0
+		ali = ai-bi*gam(idx+1)
+		!calc, store
+		gam(idx) = ci/ali
+		v(idx) = (u_BC - ci*u_BC - bi*v(idx+1))/ali
+		
+		!step3 backsolve
+		u_np1(1) = v(1)
+		do idx=2,I
+			u_np1(idx) = v(idx)-gam(idx)*u_np1(idx-1)
+		end do
+		u_n = u_np1
+	end do
+	u_out = u_n(1:I)
+	
+	!form A and f
+	
+	!do idx = 1,I
+	!	do jdx = 1,I
+	!		A(idx,jdx) = 0d0
+	!	end do
+	!end do
+	
+	!A(1,1) = 1
+	!A(1,2) = -alph*(c*dt/(2*dx))
+	!A(I,I) = 1
+	!A(I,I-1) = alph*(c*dt/(2*dx))
+	!do idx = 2,I-1
+	!	A(idx,idx) = 1d0 !ai
+	!	A(idx,idx-1) = alph*(c*dt/(2*dx))
+	!	A(idx,idx+1) = -alph*(c*dt/(2*dx))
+	!end do
+	!
+	!!Note f is RHS of Au = f. Not quite equivelent to f_i defined in maccormick
+	!!ordering is odd in maccormick. [I to 1]?
+	!f(1) = u_n(idx)-(1-alph)*c*dt/(2*dx)*(u_n(idx+1)-u_n(idx-1))+alph*(c*dt/(2*dx))*u_BC
+	!f(I) = u_n(idx)
+	!do idx = 2,I
+	!	f(idx) = u_n(idx) - (1-alph)*c*dt/(2*dx)*(u_n(idx+1)-u_n(idx-1))
+	!end do
+
+	!from here would solve. not sure this explicit writing out of matricies is necc
+
+	!u_np1(idx) = u_n(idx) - (1-alph)*c*dt/2/dx*(u_n(idx+1)-u_n(idx-1)) &
+	! - alph*c*dt/2/dx*(u_np1(idx+1)-u_np1(idx-1))
+	
+	!Form matrix A !may not need to do explicitly (but dont have book infront of me)
+	!good check of the thomas alg. Also would need to import lappack, which s a good exercise.
 	
 end function implicit_alph
 
@@ -41,7 +130,7 @@ subroutine output_result(path, x, u_IC, u)
 	end do
 end subroutine output_result
 
-program wave_eqn_ex
+program wave_eqn_imp
 	implicit none
 	
 	!io
@@ -61,13 +150,12 @@ program wave_eqn_ex
 	real :: dx, dt1, dt2
 	
 	interface !is this really the best way to do this? Lots of repeated code.
-		function implicit_alph(x, dt, N, u_IC, u_BC, c, alph) result(u_np1)
+		function implicit_alph(x, dt, N, u_IC, u_BC, c, alph) result(u_n)
 			real, dimension(1:), intent(in) :: x, u_IC
 			real, intent(in) 				:: dt, u_BC, c, alph
 			integer, intent(in) 			:: N
-			real, dimension(size(x))		:: u_np1
+			real, dimension(size(x))		:: u_n
 		end function
-		
 		subroutine output_result(path, x, u_IC, u)
 			character(*), intent(in) :: path
 			real, dimension(1:), intent(in) :: x
@@ -116,12 +204,16 @@ program wave_eqn_ex
 	call output_result("exact_soln1.dat",x,u_IC,u_exact1)
 	call output_result("exact_soln2.dat",x,u_IC,u_exact2)
 	
-	!method 4 fully implicit, central
+	!method 4 fully implicit, central (alph=1.0d0)
 	u = implicit_alph(x, dt1, N, u_IC, u0, c, 1.0e0)
 	call output_result("implicit_central.dat", x, u_IC, u)
+	u = implicit_alph(x, dt2, N, u_IC, u0, c, 1.0e0)
+	call output_result("implicit_central_CFL2.dat", x, u_IC, u)
 	
-	!method 5 crank-nicholson (implicit)
+	!method 5 crank-nicholson (implicit) (alph=0.5d0)
 	u = implicit_alph(x, dt1, N, u_IC, u0, c, 0.5e0)
 	call output_result("crank_nich.dat", x, u_IC, u)
+	u = implicit_alph(x, dt2, N, u_IC, u0, c, 0.5e0)
+	call output_result("crank_nich_CFL2.dat", x, u_IC, u)
 	
-end program wave_eqn_ex
+end program wave_eqn_imp
